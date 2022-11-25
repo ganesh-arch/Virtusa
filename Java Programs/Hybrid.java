bf846e7b52e14159cd2ed42b2fb7af09d33d0bcc
Ganesh Reddy <61350610+ganesh-arch@users.noreply.github.com>
2022-11-26 00:07:15 +0530

    Create Hybrid.java

---
+++ Java Programs/Hybrid.java
@@ -0,0 +1,44 @@
+//Single and Multiple Inheritance
+class HumanBody  
+{  
+public void displayHuman()  
+{  
+System.out.println("Method defined inside HumanBody class");  
+}  
+}  
+interface Male  
+{  
+public void show();  
+}  
+interface Female  
+{  
+public void show();  
+}   
+public class Child extends HumanBody implements Male, Female  
+{  
+public void show()  
+{  
+System.out.println("Implementation of show() method defined in interfaces Male and Female");  
+}    
+public void displayChild()  
+{  
+System.out.println("Method defined inside Child class");  
+}   
+public static void main(String args[])  
+{  
+Child obj = new Child();  
+System.out.println("Implementation of Hybrid Inheritance in Java");  
+obj.show();  
+obj.displayChild();  
+}  
+}  
+
+/*
+
+output:
+
+Implementation of Hybrid Inheritance in Java
+Implementation of show() method defined in interfaces Male and Female
+Method defined inside Child class
+
+*/
